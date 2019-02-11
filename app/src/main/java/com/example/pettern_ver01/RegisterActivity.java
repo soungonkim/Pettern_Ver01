7fff35096e51f4797f6c90863a66789b24a4dc85
soungonkim <soungonkim@kaist.ac.kr>
2019-02-11 11:01:56 +0900

    first

--- app/src/main/java/com/example/pettern_ver01/RegisterActivity.java
+++ app/src/main/java/com/example/pettern_ver01/RegisterActivity.java
@@ -17,7 +17,7 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
     private static final String KEY_STATUS = "status";
     private static final String KEY_MESSAGE = "message";
     private static final String KEY_FULL_NAME = "full_name";
@@ -26,53 +26,71 @@ public class MainActivity extends AppCompatActivity {
     private static final String KEY_EMPTY = "";
     private EditText etUsername;
     private EditText etPassword;
+    private EditText etConfirmPassword;
+    private EditText etFullName;
     private String username;
     private String password;
+    private String confirmPassword;
+    private String fullName;
     private ProgressDialog pDialog;
-    private String login_url = "http://192.168.0.13/members/login.php";
+    private String register_url = "http://192.168.0.13/members/register.php";
     private SessionHandler session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         session = new SessionHandler(getApplicationContext());
+        setContentView(R.layout.activity_register);
 
-        if(session.isLoggedIn()){
-            loadDashboard();
-        }
-        setContentView(R.layout.activity_main);
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        etConfirmPassword = findViewById(R.id.etConfirmPassword);
+        etFullName = findViewById(R.id.etFullName);
 
-        etUsername = findViewById(R.id.etLoginUsername);
-        etPassword = findViewById(R.id.etLoginPassword);
+        Button login = findViewById(R.id.btnRegisterLogin);
+        Button register = findViewById(R.id.btnRegister);
 
-        Button register = findViewById(R.id.btnLoginRegister);
-        Button login = findViewById(R.id.btnLogin);
-
-        //Launch Registration screen when Register Button is clicked
-        register.setOnClickListener(new View.OnClickListener() {
+        //Launch Login screen when Login Button is clicked
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, RegisterActivity.class);
+                Intent i = new Intent(RegisterActivity.this, MainActivity.class);
                 startActivity(i);
                 finish();
             }
         });
 
-        login.setOnClickListener(new View.OnClickListener() {
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Retrieve the data entered in the edit texts
                 username = etUsername.getText().toString().toLowerCase().trim();
                 password = etPassword.getText().toString().trim();
+                confirmPassword = etConfirmPassword.getText().toString().trim();
+                fullName = etFullName.getText().toString().trim();
                 if (validateInputs()) {
-                    login();
+                    registerUser();
                 }
+
             }
         });
+
     }
 
     /**
-     * Launch Dashboard Activity on Successful Login
+     * Display Progress bar while registering
+     */
+    private void displayLoader() {
+        pDialog = new ProgressDialog(RegisterActivity.this);
+        pDialog.setMessage("Signing Up.. Please wait...");
+        pDialog.setIndeterminate(false);
+        pDialog.setCancelable(false);
+        pDialog.show();
+
+    }
+
+    /**
+     * Launch Dashboard Activity on Successful Sign Up
      */
     private void loadDashboard() {
         Intent i = new Intent(getApplicationContext(), DashboardActivity.class);
@@ -81,41 +99,34 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /**
-     * Display Progress bar while Logging in
-     */
-
-    private void displayLoader() {
-        pDialog = new ProgressDialog(MainActivity.this);
-        pDialog.setMessage("Logging In.. Please wait...");
-        pDialog.setIndeterminate(false);
-        pDialog.setCancelable(false);
-        pDialog.show();
-
-    }
-
-    private void login() {
+    private void registerUser() {
         displayLoader();
         JSONObject request = new JSONObject();
         try {
             //Populate the request parameters
             request.put(KEY_USERNAME, username);
             request.put(KEY_PASSWORD, password);
+            request.put(KEY_FULL_NAME, fullName);
 
         } catch (JSONException e) {
             e.printStackTrace();
         }
         JsonObjectRequest jsArrayRequest = new JsonObjectRequest
-                (Request.Method.POST, login_url, request, new Response.Listener<JSONObject>() {
+                (Request.Method.POST, register_url, request, new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         pDialog.dismiss();
                         try {
-                            //Check if user got logged in successfully
-
+                            //Check if user got registered successfully
                             if (response.getInt(KEY_STATUS) == 0) {
-                                session.loginUser(username,response.getString(KEY_FULL_NAME));
+                                //Set the user session
+                                session.loginUser(username,fullName);
                                 loadDashboard();
+
+                            }else if(response.getInt(KEY_STATUS) == 1){
+                                //Display error message if username is already existsing
+                                etUsername.setError("Username already taken!");
+                                etUsername.requestFocus();
 
                             }else{
                                 Toast.makeText(getApplicationContext(),
@@ -148,16 +159,34 @@ public class MainActivity extends AppCompatActivity {
      * @return
      */
     private boolean validateInputs() {
-        if(KEY_EMPTY.equals(username)){
+        if (KEY_EMPTY.equals(fullName)) {
+            etFullName.setError("Full Name cannot be empty");
+            etFullName.requestFocus();
+            return false;
+
+        }
+        if (KEY_EMPTY.equals(username)) {
             etUsername.setError("Username cannot be empty");
             etUsername.requestFocus();
             return false;
         }
-        if(KEY_EMPTY.equals(password)){
+        if (KEY_EMPTY.equals(password)) {
             etPassword.setError("Password cannot be empty");
             etPassword.requestFocus();
             return false;
         }
+
+        if (KEY_EMPTY.equals(confirmPassword)) {
+            etConfirmPassword.setError("Confirm Password cannot be empty");
+            etConfirmPassword.requestFocus();
+            return false;
+        }
+        if (!password.equals(confirmPassword)) {
+            etConfirmPassword.setError("Password and Confirm Password does not match");
+            etConfirmPassword.requestFocus();
+            return false;
+        }
+
         return true;
     }
 }
